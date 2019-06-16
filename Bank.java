2ad4dea5a2575d49638ff6ab09ba929ff555bd12
megha1590 <meghagupta.1590@gmail.com>
2019-06-17 00:38:02 +0530

    hello this is my first time

---
+++ Bank.java
@@ -0,0 +1,31 @@
+class Bank
+
+{
+
+
+public static void main(String[] args) 
+{
+	
+
+			
+int bal=9999;
+if(bal<2000)
+{
+System.out.println("**************************************************"); 
+System.out.println("PLEASE MAINTAIN MIN BALANCE");
+System.out.println("**************************************************");
+}
+else
+{
+System.out.println("**************************************************"); 
+System.out.println("get lost");
+
+
+System.out.println("**************************************************"); 
+
+}
+
+}
+
+}
+
